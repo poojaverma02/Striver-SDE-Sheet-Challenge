dc57a6e5492222a94c1ea0377e814e3de4ec0b6c
poojaverma02 <poojaverma.ece.aec@gmail.com>
2026-07-30 20:13:52 +0530

    findMaximumXOR added

---
+++ 15-Trie/Day 59/findMaximumXOR.cpp
@@ -0,0 +1,111 @@
+// T.C: O(32N + 32M)
+    // Insertion Phase (O(32N))
+    // Query Phase (O(32M))
+// S.C: O(32N)
+// Question Link: https://takeuforward.org/plus/dsa/problems/maximum-xor-of-two-numbers-in-an-array?source=strivers-sde-sheet
+
+
+struct Node {
+    Node* links[2]; 
+    
+    /*To check if a specific bit 
+    key is present in the child nodes*/
+    bool containsKey(int bit) {
+        return (links[bit] != NULL);
+    }
+    
+    /*To get child node 
+    corresponding to a 
+    specific bit*/
+    Node* get(int bit) {
+        return links[bit];
+    }
+    
+    /*To set a child node at a specific 
+    index in the links array*/
+    void put(int bit, Node* node) {
+        links[bit] = node;
+    }
+};
+
+class Trie {
+private:
+    Node* root; 
+
+public:
+    // Constructor 
+    Trie() {
+        root = new Node();
+    }
+    
+    // To insert number in Trie
+    void insert(int num) {
+        // Start from the root
+        Node* node = root;
+        // Iterate each bit
+        for (int i = 31; i >= 0; i--) {
+            // Extract i-th bit
+            int bit = (num >> i) & 1;
+           /* If the current node doesn't 
+           have a child node with the 
+           current bit*/
+            if (!node->containsKey(bit)) {
+                node->put(bit, new Node());
+            }
+            node = node->get(bit);
+        }
+    }
+    
+    // Method to find the maximum XOR 
+    int getMax(int num) {
+        // Start from root
+        Node* node = root;
+        // Initialize maximum XOR value
+        int maxNum = 0;
+        // Iterate each bit
+        for (int i = 31; i >= 0; i--) {
+            // Extract the i-th bit 
+            int bit = (num >> i) & 1;
+            /*If the complement of the current bit 
+            exists in the Trie update the maximum 
+            XOR value with the current bit
+            move to the child node corresponding to 
+            the complement of the current bit
+            Else move to the child node 
+            corresponding to the current bit*/
+            if (node->containsKey(1 - bit)) {
+                
+                maxNum |= (1 << i);
+                
+                node = node->get(1 - bit);
+            } else {
+                node = node->get(bit);
+            }
+        }
+        // Return maximum XOR
+        return maxNum;
+    }
+};
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        // Create Trie
+        Trie trie;
+        // Insert number
+        for (int num : nums) {
+            trie.insert(num);
+        }
+        
+        // Maximum XOR value
+        int maxi = 0;
+        
+        // Iterate each number
+        for (int num : nums) {
+            // Update the maximum XOR
+            maxi = max(maxi, trie.getMax(num));
+        }
+        
+        // Return maximum XOR value
+        return maxi;
+    }
+};
